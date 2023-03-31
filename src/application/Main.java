2b4308657a3b89b420285784965984ad7326efdf
CUEILLE Robin <robin.cueille@soprasteria.com>
2023-03-31 09:01:12 +0200

    Le Projet et mise en forme générale

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
+import javafx.scene.image.Image;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,10 +13,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("VuePrincipale.fxml"));
-			Scene scene = new Scene(root,400,400);
-			primaryStage.setTitle("Titre de la page");
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("IHM.fxml"));
+			Scene scene = new Scene(root,500,400);
+			Image image = new Image("file:Images/logo3il.png");
+			primaryStage.setTitle("Gestionnaire de couleurs");
+			primaryStage.setResizable(false);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(image);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
